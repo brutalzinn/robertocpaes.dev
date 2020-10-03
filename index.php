26856ac6b813cbe9d14e2dc7583143408112534a
displa88 <gordododelphi@gmail.com>
2020-10-03 16:26:10 -0300

    dev init

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>OnePage</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
+	<body>
+
+		<script type="text/javascript" src="js/main.js"></script>
+	</body>
+</html>

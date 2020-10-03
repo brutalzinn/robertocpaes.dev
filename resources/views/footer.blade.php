4570ff5890f31b3186172907542985d551046296
displa88 <gordododelphi@gmail.com>
2020-10-03 16:31:23 -0300

    commit

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src=""></script>
+</head>
+<body>
+    <div class="foot">
+        <img src="{{ asset('images/git.png') }}" alt="">
+        <img src="{{ asset('images/gmail.png') }}" alt="">
+    </div>
+</body>
+</html>

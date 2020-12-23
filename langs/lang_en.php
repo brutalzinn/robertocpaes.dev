0249d62308a9d6bcb802ee5420cceeca0bcabdc6
DESKTOP-4DV71MT\rober <gordododelphi@gmail.com>
2020-12-23 15:31:53 -0300

    no message

---
+++ langs/lang_en.php
@@ -0,0 +1,21 @@
+<? php
+
+define ("_DESCRIPTION", "I am self-taught. I live in Rio de Janeiro, Brazil. I was introduced to the world of programming in 2012, through Minecraft and its glorious world of blocks.");
+define ("_TITLE_MESSAGE", "An aspiring developer");
+define ("COLUMN_LANGUAGES", "LANGUAGES");
+define ("COLUMN_INFRA", "INFRASTRUCTURE");
+define ("COLUMN_DB", "DATABASE");
+define ("COLUMN_ENERGY", "CREATIVE ENERGY");
+define ("COLUMN_COMMUNICATION", "COMMUNICATION");
+define ("COLUMN_COUCH", "ETERNAL LEARNING");
+define ("COLUMN_SOCIAL_NETWORK", "SOCIAL NETWORKS");
+
+define ("COLUMN_ENERGY_RESPONSE", "The energy that moves me is my fascination with solving increasingly interesting problems. My curiosity for the answer always takes me out of inertia.
+");
+define ("COLUMN_COMMUNICATION_RESPONSE", "I understand the fundamental role of communication in the development of softaware. And I will do my best to meet the demands requested judiciously.
+");
+define ("COLUMN_COUCH_RESPONSE", "I'm always willing to listen. Being a developer means reinventing yourself whenever necessary. This includes always being willing to learn something new.
+");
+
+
+?>
---
+++ langs/lang_pt.php
@@ -0,0 +1,21 @@
+<?php
+
+define("_DESCRIPTION", "Eu sou autodidata. Moro no Rio de janeiro, Brasil. Fui introduzido ao mundo da programação em 2012, através do Minecraft e o seu glorioso mundo de blocos.");
+define("_TITLE_MESSAGE", "Um desenvolvedor aspirante");
+define("COLUMN_LANGUAGES", "LINGUAGENS");
+define("COLUMN_INFRA", "INFRAESTURURA");
+define("COLUMN_DB", "BANCO DE DADOS");
+define("COLUMN_ENERGY", "ENERGIA CRIATIVA");
+define("COLUMN_COMMUNICATION", "COMUNICAÇÃO");
+define("COLUMN_COUCH", "ETERNO APRENDIZ");
+define("COLUMN_SOCIAL_NETWORK", "REDES SOCIAIS");
+
+define("COLUMN_ENERGY_RESPONSE", "A energia que me move é o meu fascínio por resolver problemas cada vez mais interessantes. A minha curiosidade pela resposta sempre me tira da inércia. 
+");
+define("COLUMN_COMMUNICATION_RESPONSE", "Eu entendo o papel fundamental da comunicação no desenvolvimento de softaware. E darei o meu melhor para atender as demandas solicitadas criteriosamente.
+");
+define("COLUMN_COUCH_RESPONSE", "Estou sempre disposto a ouvir.  Ser desenvolvedor significa se reinventar sempre que for necessário. Isso inclui sempre estar disposto a aprender algo novo.      
+");
+
+
+?>

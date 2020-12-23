6e3d8404659b75e3f4cf8d65a31f2a33fdf1dce5
DESKTOP-4DV71MT\rober <gordododelphi@gmail.com>
2020-12-23 12:41:54 -0300

    no message

--- index.php
+++ index.php
@@ -12,13 +12,14 @@
 
 <div class="header">
   <main>
+    <div class="container">
   <h1 class="title">Roberto <span class="sobrenome"> Paes </span></h1>
   <h4 class="blue">Um desenvolvedor aspirante.</h4>
 </div>
 <p class="description">
 Eu sou autodidata. Moro no Rio de janeiro, Brasil. Fui introduzido ao mundo da programação em 2012, através do Minecraft e o seu glorioso mundo de blocos.
 </p>
-<div class="container">
+
 
 <div class="grid skills">
 

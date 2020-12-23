2060263c9638b47ea686daaca74856e49c24ba0d
DESKTOP-4DV71MT\rober <gordododelphi@gmail.com>
2020-12-23 12:14:10 -0300

    no message

--- index.php
+++ index.php
@@ -29,6 +29,7 @@ testando descrição.
   <li>JAVa</li>
     <li>JAVASCRIPT</li>
     <li>C++</li>
+    <li>NodeJs</li>
 </ul>
   </div>
   
@@ -41,9 +42,13 @@ testando descrição.
   </div>
   
   <div class="column">
-    <h2>Column</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo tincidunt eros, eget luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis.</p>
-  </div>
+  <p class="blue">Banco de dados</p>
+  <ul>
+    <li>MySql</li>
+</ul>
+</div>
+
+</div>
 </div>
 </div>
 </main>

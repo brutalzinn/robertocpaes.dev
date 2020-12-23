585854f5a55ea0ec0ed44154fd3d16b9bcb8dbe4
DESKTOP-4DV71MT\rober <gordododelphi@gmail.com>
2020-12-23 12:01:05 -0300

    no message

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 </div>
 
 
-<div class="row">
+<div class="grid skills">
 
   <div class="column">
     <p class="blue">Linguagens</p>
@@ -38,6 +38,7 @@
     <h2>Column</h2>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo tincidunt eros, eget luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis.</p>
   </div>
+</div>
 </div>
 
 </body>

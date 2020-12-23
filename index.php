b3444e65f92cc626160fd4832fc3dc90c454b86a
DESKTOP-4DV71MT\rober <gordododelphi@gmail.com>
2020-12-23 12:29:20 -0300

    no message

--- index.php
+++ index.php
@@ -55,26 +55,26 @@ Eu sou autodidata. Moro no Rio de janeiro, Brasil. Fui introduzido ao mundo da p
   <div class="column">
     <p class="blue">ENERGIA CRIATIVA</p>
     <ul>
- <p>   A energia que me move é o meu fascínio por resolver problemas cada vez mais interessantes. A minha curiosidade pela resposta sempre me tira da inércia. 
-</p> 
+   A energia que me move é o meu fascínio por resolver problemas cada vez mais interessantes. A minha curiosidade pela resposta sempre me tira da inércia. 
+
 </ul>
 </div>
   
   <div class="column">
   <p class="blue">COMUNICAÇÃO</p>
   <ul>
-  <p>
+  
           Eu entendo o papel fundamental da comunicação no desenvolvimento de softaware. E darei o meu melhor para atender as demandas solicitadas criteriosamente.
-          </p>
+          
 </ul>
   </div>
   
   <div class="column">
   <p class="blue">ETERNO APRENDIZ</p>
   <ul>
-  <p>
+  
  Estou sempre disposto a ouvir.  Ser desenvolvedor significa se reinventar sempre que for necessário. Isso inclui sempre estar disposto a aprender algo novo.      
-</p>
+
 </ul>
 </div>
 

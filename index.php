041fd2337d27d08246c90bf0593c408896c0a91d
DESKTOP-4DV71MT\rober <gordododelphi@gmail.com>
2020-12-23 17:24:58 -0300

    no message

--- index.php
+++ index.php
@@ -21,13 +21,15 @@ if(isset($_SESSION['lang'])){
 <html lang="pt">
 <head>
 <title>Roberto Paes - portfólio</title>
-<meta charset="utf-8">
+<meta content='text/html; charset=UTF-8' http-equiv='Content-Type'/>
+
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <link rel="stylesheet" href="css/styles.css">
 
 </head>
+
 <body>
 <script>
  function changeLang(){

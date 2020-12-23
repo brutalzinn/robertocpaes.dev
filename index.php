7f683f3f48812d27e7ec97103921ec313dc19884
DESKTOP-4DV71MT\rober <gordododelphi@gmail.com>
2020-12-23 13:32:13 -0300

    no message

--- index.php
+++ index.php
@@ -87,8 +87,8 @@ Eu sou autodidata. Moro no Rio de janeiro, Brasil. Fui introduzido ao mundo da p
   <div class="column">
     <p class="blue">Redes sociais</p>
     <ul>
-<span class="listStyleHome">></span> <a href="https://github.com/brutalzinn" ><i style="font-size:24px" class="fa">&#xf09b;</i></a><br>
-<span class="listStyleHome">></span> <a href="https://www.facebook.com/brutalzinn.roberto/" ><i style="font-size:24px" class="fa">&#xf09a;</i></a><br>
+<span class="listStyleHome">></span> <a href="https://github.com/brutalzinn" ><i style="font-size:24px" class="fa">&#xf09b;</i> Github</a><br>
+<span class="listStyleHome">></span> <a href="https://www.facebook.com/brutalzinn.roberto/" ><i style="font-size:24px" class="fa">&#xf09a;</i> Facebook</a><br>
 <span class="listStyleHome">></span><i style="font-size:24px" class="fa">&#xf0e0;</i> contato@robertocpaes.dev</p>
 
 </ul>
